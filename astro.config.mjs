3e69acece228fabe441849deaa58f706a68829ae
Ample1827 <Adalromero99@gmail.com>
2025-04-11 21:19:04 -0600

    gitpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/Ample1827/Jurassic-Bytes",
+  site: "https://github.com/Ample1827/Jurassic-Bytes.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
